f8499898e0c3ab04a4afeb282b47927d86c0fb2f
Daniel Tamayo Mela <danitamayo.adhoc@gmail.com>
2026-06-11 16:28:08 +0200

    fix: Fix alce evaluation

--- eval_alce.py
+++ eval_alce.py
@@ -317,7 +317,10 @@ def compute_autoais(data,
     global autoais_model, autoais_tokenizer
     if autoais_model is None:
         logger.info("Loading AutoAIS model...")
-        autoais_model = AutoModelForSeq2SeqLM.from_pretrained(AUTOAIS_MODEL, torch_dtype=torch.bfloat16, max_memory=get_max_memory(), device_map="auto")
+        autoais_model = AutoModelForSeq2SeqLM.from_pretrained(AUTOAIS_MODEL, 
+            torch_dtype=torch.bfloat16, 
+            device_map="auto",
+            max_memory={i: "12GiB" for i in range(torch.cuda.device_count())},)
         autoais_tokenizer = AutoTokenizer.from_pretrained(AUTOAIS_MODEL, use_fast=False)
 
     logger.info(f"Running AutoAIS...")
